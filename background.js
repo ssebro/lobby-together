cb11f5766b9d45e91c2c6c4449a1cba1b619964c
Stephen Sebro <stephen@brownianm.com>
2012-04-28 17:20:51 -0700

    First basic version that works that I'm happy with. It querries the server for the blacklist so we don't have to.

--- background.js
+++ background.js
@@ -2,29 +2,44 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+var SHOULD_LIMIT_REQUESTS=true;
 chrome.extension.onRequest.addListener(
   function(request, sender, sendResponse) {
     if (request.blacklist)
     {
-      sendResponse(
-        {
-          blacklist: getBlacklist()
-        });
-      }
+      getBlacklist(function(blacklist)
+      {
+        sendResponse(
+          {
+            blacklist: blacklist
+          });
+      });
+    }
   }
 );
 
+var self=this;
+function getBlacklist(callback){
 
-function getBlacklist(){
-  var blacklist = 
-  {
-    ".reddit.com":"This website has been disabled to raise awareness about CISPA. For more info, check out <a href='http://cyberspying.eff.org/'> the EFF </a>"
-  };
-  //might want to ask our webservice for a blacklist every day, and save that blacklist in localstorage.
-  //Our "webservice" could be something simple, like a jekyll/node.js site that ONLY serves the static blacklist, and updates 
-  //are done by hand.
-  //Could also be a json file served by github. :)
-  return blacklist;
+  if(self.blacklist)
+    callback(self.blacklist);
+
+  $.ajax({
+    url: "http://ssebro.github.com/lobby-together/blacklist.json",
+success: function(data) 
+      { 
+        callback(data);
+        if(SHOULD_LIMIT_REQUESTS)
+        {
+          //essentially rate-limiting these requests.
+          self.blacklist=data;
+          setTimeout(function(){
+            self.blacklist=undefined;
+          },60000000);
+        }
+      }
+  });
+  //might want to ask our webservice for a blacklist every day, and save that blacklist in localstorage, since the current approach is ... costly.
 }
 // Called when the url of a tab changes.
 function checkForValidUrl(tabId, changeInfo, tab) {
