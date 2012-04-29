22fd9bd5fc4138ed68f952146221c9cd6e4b0d47
Stephen Sebro <stephen@brownianm.com>
2012-04-28 17:30:22 -0700

    Added ratelimiting

--- background.js
+++ background.js
@@ -2,7 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-var SHOULD_LIMIT_REQUESTS=true;
+var SHOULD_LIMIT_REQUEST_FREQUENCY=true;
+var self=this;
+self.blacklist=undefined;
+
 chrome.extension.onRequest.addListener(
   function(request, sender, sendResponse) {
     if (request.blacklist)
@@ -18,38 +21,40 @@ chrome.extension.onRequest.addListener(
   }
 );
 
-var self=this;
 function getBlacklist(callback){
 
   if(self.blacklist)
+  {
     callback(self.blacklist);
-
+    return;
+  }
   $.ajax({
     url: "http://ssebro.github.com/lobby-together/blacklist.json",
 success: function(data) 
       { 
         callback(data);
-        if(SHOULD_LIMIT_REQUESTS)
+        if(SHOULD_LIMIT_REQUEST_FREQUENCY)
         {
-          //essentially rate-limiting these requests.
+          //essentially rate-limiting these requests, so we only do 1 every 10 hours
           self.blacklist=data;
           setTimeout(function(){
             self.blacklist=undefined;
-          },60000000);
+          },600000);
         }
       }
   });
-  //might want to ask our webservice for a blacklist every day, and save that blacklist in localstorage, since the current approach is ... costly.
 }
+
 // Called when the url of a tab changes.
 function checkForValidUrl(tabId, changeInfo, tab) {
-  // If the letter 'g' is found in the tab's URL...
-  console.log(tab);
-  console.log(changeInfo);
+  if(self.blacklist)
+  {
+    //This should really check for whether or not the url is blacklisted, but that doesn't matter for now.
 
-  if (tab.url.indexOf('reddit.com') > -1) {
-    // ... show the page action.
-    chrome.pageAction.show(tabId);
+    // if (tab.url.indexOf('reddit.com') > -1) {
+    //   // ... show the page action.
+    //   chrome.pageAction.show(tabId);
+    // }
   }
 };
 
